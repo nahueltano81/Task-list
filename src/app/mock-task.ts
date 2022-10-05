16147e7ce126b4fb13a05598557fd729a915e7c5
nahueltano81 <nahueltano81@hotmail.com>
2022-10-05 18:43:41 -0300

    Agregar mock de tareas y le interface de la misma

---
+++ src/app/mock-task.ts
@@ -0,0 +1,28 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Cagar",
+        day: '23 de Agosto 2022',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Viajar",
+        day: '23 de Marzo 2022',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "volaar",
+        day: '23 de Septe 2022',
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "Leer",
+        day: '23 de 5 de Octubre 2022',
+        reminder: false
+    },
+]
